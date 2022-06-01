d8330ab07cc077774fb375f7aec23a2a949a5f2b
Olguta Fratila <fratilaolguta150@gmail.com>
2022-06-01 23:19:34 +0300

    Finish homepage. Add a description. Add a programm.

--- src/home-page.js
+++ src/home-page.js
@@ -6,6 +6,19 @@ export function homePage() {
     const subtitle = document.createElement('h5');
     const homePage = document.createElement('div');
     const description = document.createElement('div');
+    const programme = document.createElement('div');
+    const hours = document.createElement('div');
+    const footer = document.createElement('div');
+    const a = document.createElement('a');
+
+    const newUl = document.createElement('ul');
+    const firstLi = document.createElement('li');
+    const secondLi = document.createElement('li');
+    const thirdLi = document.createElement('li');
+    const fourthLi = document.createElement('li');
+    const fifthLi = document.createElement('li');
+    const sixthLi = document.createElement('li');
+    const seventhLi = document.createElement('li');
 
     title.textContent = `At Lola's`;
     title.classList.add('title');
@@ -15,10 +28,39 @@ export function homePage() {
     
     homePage.classList.add('home-page');
     description.classList.add('description');
-    description.textContent = `At Lola's specializes in delicious food featuring fresh ingredients and masterful preparation by the Lola culinary team. Whether you're ordering a multi-course meal or grabbing a drink and pizza at the bar, Lola's lively, casual yet upscale atmosphere makes it perfect for dining with friends, family, clients and business associates.`;
+    description.textContent = `At Lola's specializes in delicious food featuring fresh ingredients and masterful preparation by the Lola's culinary team. Whether you're ordering a multi-course meal or grabbing a drink and pizza at the bar, Lola's lively, casual yet upscale atmosphere makes it perfect for dining with friends, family, clients and business associates.`;
+
+    programme.classList.add('programme');
+    hours.textContent = 'Hours';
+    hours.classList.add('hours');
+
+    firstLi.textContent = 'Monday: 10am - 10pm';
+    secondLi.textContent = 'Tuesday: 10am - 10pm';
+    thirdLi.textContent = 'Wednesday: 10am - 10pm';
+    fourthLi.textContent = 'Thursday: 10am - 10pm';
+    fifthLi.textContent = 'Friday: 10am - 10pm';
+    sixthLi.textContent = 'Saturday: 11am - 11pm';
+    seventhLi.textContent = 'Sunday: 11am - 11pm';
+
+    footer.classList = 'footer';
+    a.textContent = `Background image created by 
+    Nick Karvounis`;
+    a.href = 'https://unsplash.com/photos/Ciqxn7FE4vE';
 
     content.appendChild(title);
     content.appendChild(subtitle);
     body.appendChild(homePage);
     homePage.appendChild(description);
+    homePage.appendChild(programme);
+    programme.appendChild(hours);
+    programme.appendChild(newUl);
+    newUl.appendChild(firstLi);
+    newUl.appendChild(secondLi);
+    newUl.appendChild(thirdLi);
+    newUl.appendChild(fourthLi);
+    newUl.appendChild(fifthLi);
+    newUl.appendChild(sixthLi);
+    newUl.appendChild(seventhLi);
+    homePage.appendChild(footer);
+    footer.appendChild(a);
 }
