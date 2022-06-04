5184c257b963c49b59bb11c351217878917ad67f
Olguta Fratila <fratilaolguta150@gmail.com>
2022-06-04 07:20:13 +0300

    Add header with home, menu and contact buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 import {homePage} from './home-page.js';
+import {menuPage} from './menu-page';
 
 console.log('New Restaurant Page');
-homePage();
+// homePage();
+menuPage();
