fab55a6d45c6721fce6befa6872f9ea581ae9c0a
Olguta Fratila <fratilaolguta150@gmail.com>
2022-06-04 23:13:11 +0300

    Edited

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 import {homePage} from './home-page.js';
 import {menuPage} from './menu-page';
+import {contactPage} from './contact-page';
 
 console.log('New Restaurant Page');
 // homePage();
-menuPage();
+// menuPage();
+contactPage();
