37b97f79607bd423ca6eb075a86db6ffaf00863b
Olguta Fratila <fratilaolguta150@gmail.com>
2022-06-06 08:33:59 +0300

    Add the function for switching between tabs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import './style.css';
 import {homePage} from './home-page.js';
-import {menuPage} from './menu-page';
-import {contactPage} from './contact-page';
+import {menuPage} from './menu-page.js';
+import {contactPage} from './contact-page.js';
 
-console.log('New Restaurant Page');
-// homePage();
-// menuPage();
-contactPage();
+homePage();
+
+let switchingModules = (function() {
+
+    const homeTab = document.querySelector('.home');
+    homeTab.addEventListener('click', homePage);
+    
+    
+    const menuTab = document.querySelector('.menu');
+    menuTab.addEventListener('click', menuPage);
+    
+    const contactTab = document.querySelector('.contact');
+    contactTab.addEventListener('click', contactPage);
+
+})();
